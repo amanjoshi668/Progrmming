939205c15a021cf52373613f8c641e43e8fb842d
Aman Joshi <amanjoshi668@gamil.com>
2020-01-20 12:22:50 +0530

    timely commit

---
+++ codeforces/614-2/D.cpp
@@ -0,0 +1,696 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+//#include <ext/pb_ds/assoc_container.hpp> // Common file
+//#include <ext/pb_ds/tree_policy.hpp> // Including seg_tree_order_statistics_node_update
+#include <stdio.h>
+#include <cassert>
+using namespace std;
+//using namespace __gnu_pbds;
+typedef long long lo;
+typedef long double ld;
+#include <ctime>
+typedef pair<lo, lo> ll; //pair
+typedef vector<lo> vl;   //vector of long
+typedef vector<ll> vll;  //vector of pair
+typedef priority_queue<lo> p_q;
+typedef vector<vl> vvl; //vector of vectors
+#define X first
+#define Y second
+#define mp(a, b) make_pair((a), (b))
+#define REP(a, b) for (lo i = (a); i < (lo)b; i++) //no need to declare variable i
+#define REPE(a, b, c, d) \
+    REP(a, b)            \
+    for (lo j = (c); j < (lo)d; j++)                    //no need to declare vaiables i,j
+#define REPV(a, b, c) for (lo(a) = b; (a) < (c); (a)++) //a is the variable
+#define IREP(a, b) for (lo i = (a); i >= (b); i--)
+#define IREPV(a, b, c) for (lo(a) = b; (a) >= (c); (a)--)
+#define correct(x, y, n, m) (0 <= (x) && (x) < (n) && 0 <= (y) && (y) < (m))
+#define all(v) (v).begin(), (v).end()
+#define TRV(a) for (auto &it : a)
+#define INF 500010
+#define MOD 1000000007
+#define MOD2 1000000009
+#define BLOCK 300
+#define CHECK_BIT(var, pos) ((var) & (1 << (pos)))
+#define pb(a) push_back((a))
+#define eps 1e-2
+#define PI acos(-1.0)
+
+#if _DEBUG
+#define debug(x) cout << #x << "=" << x << endl
+#define debug2(x, y) cout << #x << "=" << x << " " << #y << "=" << y << endl;
+#define debug3(x, y, z) cout << #x << "=" << x << " " << #y << "=" << y << " " << #z << "=" << z << endl;
+#define debug4(x, y, z, w) cout << #x << "=" << x << " " << #y << "=" << y << " " << #z << "=" << z << " " << #w << "=" << w << endl;
+#define debug5(x, y, z, w, t)      \
+    cout << #x << "=" << x << " "; \
+    debug4(y, z, w, t);
+#define debug6(p, x, y, z, w, t)                            \
+    cout << #p << " " << p << " " << #x << "=" << x << " "; \
+    debug4(y, z, w, t);
+#define debug7(o, p, x, y, z, w, t) \
+    cout << #o << " " << o << " ";  \
+    debug6(p, x, y, z, w, t);
+#define wait  \
+    int xxxx; \
+    cin >> xxxx;
+#define derr(x) cerr << #x << "=" << x << endl;
+#define derr2(x, y) cerr << #x << "=" << x << " " << #y << "=" << y << endl;
+#define derr3(x, y, z) cerr << #x << "=" << x << " " << #y << "=" << y << " " << #z << "=" << z << endl;
+#define derr4(x, y, z, w) cerr << #x << "=" << x << " " << #y << "=" << y << " " << #z << "=" << z << " " << #w << "=" << w << endl;
+#define derr5(x, y, z, w, t)       \
+    cerr << #x << "=" << x << " "; \
+    derr4(y, z, w, t);
+#define derr6(p, x, y, z, w, t)                             \
+    cerr << #p << " " << p << " " << #x << "=" << x << " "; \
+    derr4(y, z, w, t);
+#define derr7(o, p, x, y, z, w, t) \
+    cerr << #o << " " << o << " "; \
+    derr6(p, x, y, z, w, t);
+lo checkpoint_counter = 0;
+#define checkpoint cerr << "At checkpoint : " << checkpoint_counter++ << endl;
+
+#else
+#define debug(x) ;
+#define debug2(x, y) ;
+#define debug3(x, y, z) ;
+#define debug4(x, y, z, q) ;
+#define debug5(x, y, z, r, t) ;
+#define debug6(x, y, z, r, t, s) ;
+#define debug7(x, y, z, r, t, s, u) ;
+#define wait ;
+#define derr(x) ;
+#define derr2(x, y) ;
+#define derr3(x, y, z) ;
+#define derr4(x, y, z, q) ;
+#define derr5(x, y, z, r, t) ;
+#define derr6(x, y, z, r, t, s) ;
+#define derr7(x, y, z, r, t, f, u) ;
+#define checkpoint ;
+#endif
+
+#define print_matrix(a, n, m) \
+    REPE(0, n, 0, m) { cout << (a)[i][j] << ((j == m - 1) ? '\n' : ' '); }
+#define present(container, element) (container.find(element) != container.end())
+#define endl "\n"
+template <typename T>
+ostream &operator<<(ostream &o, vector<T> v)
+{
+    if (v.size() > 0)
+        o << v[0];
+    for (unsigned i = 1; i < v.size(); i++)
+        o << " " << v[i];
+    return o << " ";
+}
+template <typename U, typename V>
+ostream &operator<<(ostream &o, pair<U, V> p)
+{
+    return o << "(" << p.first << ", " << p.second << ") ";
+}
+template <typename T>
+istream &operator>>(istream &in, vector<T> &v)
+{
+    for (unsigned i = 0; i < v.size(); i++)
+        in >> v[i];
+    return in;
+}
+template <typename T>
+istream &operator>>(istream &in, pair<T, T> &p)
+{
+    in >> p.X;
+    in >> p.Y;
+    return in;
+}
+template <typename T>
+ostream &operator<<(ostream &o, set<T> v)
+{
+    TRV(v)
+    o << it << " ";
+    return o << endl;
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &o, map<T, U> v)
+{
+    TRV(v)
+    o << it << " ";
+    return o << endl;
+}
+template <typename T>
+T &&vmin(T &&val)
+{
+    return std::forward<T>(val);
+}
+
+template <typename T0, typename T1, typename... Ts>
+auto vmin(T0 &&val1, T1 &&val2, Ts &&... vs)
+{
+    return (val1 < val2) ? vmin(val1, std::forward<Ts>(vs)...) : vmin(val2, std::forward<Ts>(vs)...);
+}
+template <typename T>
+T &&vmax(T &&val)
+{
+    return std::forward<T>(val);
+}
+
+template <typename T0, typename T1, typename... Ts>
+auto vmax(T0 &&val1, T1 &&val2, Ts &&... vs)
+{
+    return (val1 > val2) ? vmax(val1, std::forward<Ts>(vs)...) : vmax(val2, std::forward<Ts>(vs)...);
+}
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+const int base = 1000000000;
+const int base_digits = 9;
+struct bigint
+{
+    vector<int> a;
+    int sign;
+    /*<arpa>*/
+    int size()
+    {
+        if (a.empty())
+            return 0;
+        int ans = (a.size() - 1) * base_digits;
+        int ca = a.back();
+        while (ca)
+            ans++, ca /= 10;
+        return ans;
+    }
+    bigint operator^(const bigint &v)
+    {
+        bigint ans = 1, a = *this, b = v;
+        while (!b.isZero())
+        {
+            if (b % 2)
+                ans *= a;
+            a *= a, b /= 2;
+        }
+        return ans;
+    }
+    string to_string()
+    {
+        stringstream ss;
+        ss << *this;
+        string s;
+        ss >> s;
+        return s;
+    }
+    int sumof()
+    {
+        string s = to_string();
+        int ans = 0;
+        for (auto c : s)
+            ans += c - '0';
+        return ans;
+    }
+    /*</arpa>*/
+    bigint() : sign(1)
+    {
+    }
+
+    bigint(long long v)
+    {
+        *this = v;
+    }
+
+    bigint(const string &s)
+    {
+        read(s);
+    }
+
+    void operator=(const bigint &v)
+    {
+        sign = v.sign;
+        a = v.a;
+    }
+
+    void operator=(long long v)
+    {
+        sign = 1;
+        a.clear();
+        if (v < 0)
+            sign = -1, v = -v;
+        for (; v > 0; v = v / base)
+            a.push_back(v % base);
+    }
+
+    bigint operator+(const bigint &v) const
+    {
+        if (sign == v.sign)
+        {
+            bigint res = v;
+
+            for (int i = 0, carry = 0; i < (int)max(a.size(), v.a.size()) || carry; ++i)
+            {
+                if (i == (int)res.a.size())
+                    res.a.push_back(0);
+                res.a[i] += carry + (i < (int)a.size() ? a[i] : 0);
+                carry = res.a[i] >= base;
+                if (carry)
+                    res.a[i] -= base;
+            }
+            return res;
+        }
+        return *this - (-v);
+    }
+
+    bigint operator-(const bigint &v) const
+    {
+        if (sign == v.sign)
+        {
+            if (abs() >= v.abs())
+            {
+                bigint res = *this;
+                for (int i = 0, carry = 0; i < (int)v.a.size() || carry; ++i)
+                {
+                    res.a[i] -= carry + (i < (int)v.a.size() ? v.a[i] : 0);
+                    carry = res.a[i] < 0;
+                    if (carry)
+                        res.a[i] += base;
+                }
+                res.trim();
+                return res;
+            }
+            return -(v - *this);
+        }
+        return *this + (-v);
+    }
+
+    void operator*=(int v)
+    {
+        if (v < 0)
+            sign = -sign, v = -v;
+        for (int i = 0, carry = 0; i < (int)a.size() || carry; ++i)
+        {
+            if (i == (int)a.size())
+                a.push_back(0);
+            long long cur = a[i] * (long long)v + carry;
+            carry = (int)(cur / base);
+            a[i] = (int)(cur % base);
+            //asm("divl %%ecx" : "=a"(carry), "=d"(a[i]) : "A"(cur), "c"(base));
+        }
+        trim();
+    }
+
+    bigint operator*(int v) const
+    {
+        bigint res = *this;
+        res *= v;
+        return res;
+    }
+
+    void operator*=(long long v)
+    {
+        if (v < 0)
+            sign = -sign, v = -v;
+        if (v > base)
+        {
+            *this = *this * (v / base) * base + *this * (v % base);
+            return;
+        }
+        for (int i = 0, carry = 0; i < (int)a.size() || carry; ++i)
+        {
+            if (i == (int)a.size())
+                a.push_back(0);
+            long long cur = a[i] * (long long)v + carry;
+            carry = (int)(cur / base);
+            a[i] = (int)(cur % base);
+            //asm("divl %%ecx" : "=a"(carry), "=d"(a[i]) : "A"(cur), "c"(base));
+        }
+        trim();
+    }
+
+    bigint operator*(long long v) const
+    {
+        bigint res = *this;
+        res *= v;
+        return res;
+    }
+
+    friend pair<bigint, bigint> divmod(const bigint &a1, const bigint &b1)
+    {
+        int norm = base / (b1.a.back() + 1);
+        bigint a = a1.abs() * norm;
+        bigint b = b1.abs() * norm;
+        bigint q, r;
+        q.a.resize(a.a.size());
+
+        for (int i = a.a.size() - 1; i >= 0; i--)
+        {
+            r *= base;
+            r += a.a[i];
+            int s1 = r.a.size() <= b.a.size() ? 0 : r.a[b.a.size()];
+            int s2 = r.a.size() <= b.a.size() - 1 ? 0 : r.a[b.a.size() - 1];
+            int d = ((long long)base * s1 + s2) / b.a.back();
+            r -= b * d;
+            while (r < 0)
+                r += b, --d;
+            q.a[i] = d;
+        }
+
+        q.sign = a1.sign * b1.sign;
+        r.sign = a1.sign;
+        q.trim();
+        r.trim();
+        return make_pair(q, r / norm);
+    }
+
+    bigint operator/(const bigint &v) const
+    {
+        return divmod(*this, v).first;
+    }
+
+    bigint operator%(const bigint &v) const
+    {
+        return divmod(*this, v).second;
+    }
+
+    void operator/=(int v)
+    {
+        if (v < 0)
+            sign = -sign, v = -v;
+        for (int i = (int)a.size() - 1, rem = 0; i >= 0; --i)
+        {
+            long long cur = a[i] + rem * (long long)base;
+            a[i] = (int)(cur / v);
+            rem = (int)(cur % v);
+        }
+        trim();
+    }
+
+    bigint operator/(int v) const
+    {
+        bigint res = *this;
+        res /= v;
+        return res;
+    }
+
+    int operator%(int v) const
+    {
+        if (v < 0)
+            v = -v;
+        int m = 0;
+        for (int i = a.size() - 1; i >= 0; --i)
+            m = (a[i] + m * (long long)base) % v;
+        return m * sign;
+    }
+
+    void operator+=(const bigint &v)
+    {
+        *this = *this + v;
+    }
+    void operator-=(const bigint &v)
+    {
+        *this = *this - v;
+    }
+    void operator*=(const bigint &v)
+    {
+        *this = *this * v;
+    }
+    void operator/=(const bigint &v)
+    {
+        *this = *this / v;
+    }
+
+    bool operator<(const bigint &v) const
+    {
+        if (sign != v.sign)
+            return sign < v.sign;
+        if (a.size() != v.a.size())
+            return a.size() * sign < v.a.size() * v.sign;
+        for (int i = a.size() - 1; i >= 0; i--)
+            if (a[i] != v.a[i])
+                return a[i] * sign < v.a[i] * sign;
+        return false;
+    }
+
+    bool operator>(const bigint &v) const
+    {
+        return v < *this;
+    }
+    bool operator<=(const bigint &v) const
+    {
+        return !(v < *this);
+    }
+    bool operator>=(const bigint &v) const
+    {
+        return !(*this < v);
+    }
+    bool operator==(const bigint &v) const
+    {
+        return !(*this < v) && !(v < *this);
+    }
+    bool operator!=(const bigint &v) const
+    {
+        return *this < v || v < *this;
+    }
+
+    void trim()
+    {
+        while (!a.empty() && !a.back())
+            a.pop_back();
+        if (a.empty())
+            sign = 1;
+    }
+
+    bool isZero() const
+    {
+        return a.empty() || (a.size() == 1 && !a[0]);
+    }
+
+    bigint operator-() const
+    {
+        bigint res = *this;
+        res.sign = -sign;
+        return res;
+    }
+
+    bigint abs() const
+    {
+        bigint res = *this;
+        res.sign *= res.sign;
+        return res;
+    }
+
+    long long longValue() const
+    {
+        long long res = 0;
+        for (int i = a.size() - 1; i >= 0; i--)
+            res = res * base + a[i];
+        return res * sign;
+    }
+
+    friend bigint gcd(const bigint &a, const bigint &b)
+    {
+        return b.isZero() ? a : gcd(b, a % b);
+    }
+    friend bigint lcm(const bigint &a, const bigint &b)
+    {
+        return a / gcd(a, b) * b;
+    }
+
+    void read(const string &s)
+    {
+        sign = 1;
+        a.clear();
+        int pos = 0;
+        while (pos < (int)s.size() && (s[pos] == '-' || s[pos] == '+'))
+        {
+            if (s[pos] == '-')
+                sign = -sign;
+            ++pos;
+        }
+        for (int i = s.size() - 1; i >= pos; i -= base_digits)
+        {
+            int x = 0;
+            for (int j = max(pos, i - base_digits + 1); j <= i; j++)
+                x = x * 10 + s[j] - '0';
+            a.push_back(x);
+        }
+        trim();
+    }
+
+    friend istream &operator>>(istream &stream, bigint &v)
+    {
+        string s;
+        stream >> s;
+        v.read(s);
+        return stream;
+    }
+
+    friend ostream &operator<<(ostream &stream, const bigint &v)
+    {
+        if (v.sign == -1)
+            stream << '-';
+        stream << (v.a.empty() ? 0 : v.a.back());
+        for (int i = (int)v.a.size() - 2; i >= 0; --i)
+            stream << setw(base_digits) << setfill('0') << v.a[i];
+        return stream;
+    }
+
+    static vector<int> convert_base(const vector<int> &a, int old_digits, int new_digits)
+    {
+        vector<long long> p(max(old_digits, new_digits) + 1);
+        p[0] = 1;
+        for (int i = 1; i < (int)p.size(); i++)
+            p[i] = p[i - 1] * 10;
+        vector<int> res;
+        long long cur = 0;
+        int cur_digits = 0;
+        for (int i = 0; i < (int)a.size(); i++)
+        {
+            cur += a[i] * p[cur_digits];
+            cur_digits += old_digits;
+            while (cur_digits >= new_digits)
+            {
+                res.push_back(int(cur % p[new_digits]));
+                cur /= p[new_digits];
+                cur_digits -= new_digits;
+            }
+        }
+        res.push_back((int)cur);
+        while (!res.empty() && !res.back())
+            res.pop_back();
+        return res;
+    }
+
+    typedef vector<long long> vll;
+
+    static vll karatsubaMultiply(const vll &a, const vll &b)
+    {
+        int n = a.size();
+        vll res(n + n);
+        if (n <= 32)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    res[i + j] += a[i] * b[j];
+            return res;
+        }
+
+        int k = n >> 1;
+        vll a1(a.begin(), a.begin() + k);
+        vll a2(a.begin() + k, a.end());
+        vll b1(b.begin(), b.begin() + k);
+        vll b2(b.begin() + k, b.end());
+
+        vll a1b1 = karatsubaMultiply(a1, b1);
+        vll a2b2 = karatsubaMultiply(a2, b2);
+
+        for (int i = 0; i < k; i++)
+            a2[i] += a1[i];
+        for (int i = 0; i < k; i++)
+            b2[i] += b1[i];
+
+        vll r = karatsubaMultiply(a2, b2);
+        for (int i = 0; i < (int)a1b1.size(); i++)
+            r[i] -= a1b1[i];
+        for (int i = 0; i < (int)a2b2.size(); i++)
+            r[i] -= a2b2[i];
+
+        for (int i = 0; i < (int)r.size(); i++)
+            res[i + k] += r[i];
+        for (int i = 0; i < (int)a1b1.size(); i++)
+            res[i] += a1b1[i];
+        for (int i = 0; i < (int)a2b2.size(); i++)
+            res[i + n] += a2b2[i];
+        return res;
+    }
+
+    bigint operator*(const bigint &v) const
+    {
+        vector<int> a6 = convert_base(this->a, base_digits, 6);
+        vector<int> b6 = convert_base(v.a, base_digits, 6);
+        vll a(a6.begin(), a6.end());
+        vll b(b6.begin(), b6.end());
+        while (a.size() < b.size())
+            a.push_back(0);
+        while (b.size() < a.size())
+            b.push_back(0);
+        while (a.size() & (a.size() - 1))
+            a.push_back(0), b.push_back(0);
+        vll c = karatsubaMultiply(a, b);
+        bigint res;
+        res.sign = sign * v.sign;
+        for (int i = 0, carry = 0; i < (int)c.size(); i++)
+        {
+            long long cur = c[i] + carry;
+            res.a.push_back((int)(cur % 1000000));
+            carry = (int)(cur / 1000000);
+        }
+        res.a = convert_base(res.a, 6, base_digits);
+        res.trim();
+        return res;
+    }
+};
+vector<pair<bigint, bigint>> points;
+bigint distance(pair<bigint, bigint> P1, pair<bigint, bigint> P2)
+{
+    bigint res = P1.first - P2.first;
+    res = res.abs();
+    bigint other = P1.second - P2.second;
+    other = other.abs();
+    return res + other;
+}
+int main(int argc, char *argv[])
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cout.precision(20);
+    lo x, y;
+    cin >> x >> y;
+    bigint x0 = x;
+    bigint y0 = y;
+    points.push_back({x0, y0});
+    lo ax, ay, bx, by;
+    cin >> ax >> ay >> bx >> by;
+    REP(1, 300)
+    {
+        bigint nx = points.back().first * ax + bx;
+        bigint ny = points.back().second * ay + by;
+        points.push_back({nx, ny});
+    }
+    bigint sx, sy, t;
+    cin >> sx >> sy >> t;
+    lo res = 0;
+    REP(0, 200)
+    {
+        bigint dis = distance({sx, sy}, points[0]);
+        lo cnt = 0;
+        lo j = i;
+        while (dis <= t)
+        {
+            cnt++;
+            dis += distance(points[j], points[j + 1]);
+            j++;
+        }
+        res = max(res, cnt);
+        cnt = 0;
+        j = i;
+        dis = distance({sx, sy}, points[j]);
+        while (dis <= t and j > 0)
+        {
+            cnt++;
+            dis += distance(points[j], points[j - 1]);
+            j--;
+        }
+        res = max(res, cnt);
+    }
+    cout << res << endl;
+    return 0;
+}
