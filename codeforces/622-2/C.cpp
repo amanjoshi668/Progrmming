829382c2937dec93b1b41b66eb5d2a5d5a68a820
Aman Joshi <amanjoshi668@gamil.com>
2020-02-23 21:24:20 +0530

    timely push

---
+++ codeforces/622-2/C.cpp
@@ -0,0 +1,335 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+//#include <ext/pb_ds/assoc_container.hpp> // Common file
+//#include <ext/pb_ds/tree_policy.hpp> // Including seg_tree_order_statistics_node_update
+#include <stdio.h>
+#include <cassert>
+using namespace std;
+//using namespace __gnu_pbds;
+typedef long long lo;
+typedef long double ld;
+#include <ctime>
+typedef pair<lo, lo> ll; //pair
+typedef vector<lo> vl;   //vector of long
+typedef vector<ll> vll;  //vector of pair
+typedef priority_queue<lo> p_q;
+typedef vector<vl> vvl; //vector of vectors
+#define X first
+#define Y second
+#define mp(a, b) make_pair((a), (b))
+#define REP(a, b) for (lo i = (a); i < (lo)b; i++) //no need to declare variable i
+#define REPE(a, b, c, d) \
+    REP(a, b)            \
+    for (lo j = (c); j < (lo)d; j++)                    //no need to declare vaiables i,j
+#define REPV(a, b, c) for (lo(a) = b; (a) < (c); (a)++) //a is the variable
+#define IREP(a, b) for (lo i = (a); i >= (b); i--)
+#define IREPV(a, b, c) for (lo(a) = b; (a) >= (c); (a)--)
+#define correct(x, y, n, m) (0 <= (x) && (x) < (n) && 0 <= (y) && (y) < (m))
+#define all(v) (v).begin(), (v).end()
+#define TRV(a) for (auto &it : a)
+#define INF 500010
+#define MOD 1000000007
+#define MOD2 1000000009
+#define BLOCK 300
+#define CHECK_BIT(var, pos) ((var) & (1 << (pos)))
+#define pb(a) push_back((a))
+#define eps 1e-2
+#define PI acos(-1.0)
+
+#if _DEBUG
+#define debug(x) cout << #x << "=" << x << endl
+#define debug2(x, y) cout << #x << "=" << x << " " << #y << "=" << y << endl;
+#define debug3(x, y, z) cout << #x << "=" << x << " " << #y << "=" << y << " " << #z << "=" << z << endl;
+#define debug4(x, y, z, w) cout << #x << "=" << x << " " << #y << "=" << y << " " << #z << "=" << z << " " << #w << "=" << w << endl;
+#define debug5(x, y, z, w, t)      \
+    cout << #x << "=" << x << " "; \
+    debug4(y, z, w, t);
+#define debug6(p, x, y, z, w, t)                            \
+    cout << #p << " " << p << " " << #x << "=" << x << " "; \
+    debug4(y, z, w, t);
+#define debug7(o, p, x, y, z, w, t) \
+    cout << #o << " " << o << " ";  \
+    debug6(p, x, y, z, w, t);
+#define wait  \
+    int xxxx; \
+    cin >> xxxx;
+#define derr(x) cerr << #x << "=" << x << endl;
+#define derr2(x, y) cerr << #x << "=" << x << " " << #y << "=" << y << endl;
+#define derr3(x, y, z) cerr << #x << "=" << x << " " << #y << "=" << y << " " << #z << "=" << z << endl;
+#define derr4(x, y, z, w) cerr << #x << "=" << x << " " << #y << "=" << y << " " << #z << "=" << z << " " << #w << "=" << w << endl;
+#define derr5(x, y, z, w, t)       \
+    cerr << #x << "=" << x << " "; \
+    derr4(y, z, w, t);
+#define derr6(p, x, y, z, w, t)                             \
+    cerr << #p << " " << p << " " << #x << "=" << x << " "; \
+    derr4(y, z, w, t);
+#define derr7(o, p, x, y, z, w, t) \
+    cerr << #o << " " << o << " "; \
+    derr6(p, x, y, z, w, t);
+lo checkpoint_counter = 0;
+#define checkpoint cerr << "At checkpoint : " << checkpoint_counter++ << endl;
+
+#else
+#define debug(x) ;
+#define debug2(x, y) ;
+#define debug3(x, y, z) ;
+#define debug4(x, y, z, q) ;
+#define debug5(x, y, z, r, t) ;
+#define debug6(x, y, z, r, t, s) ;
+#define debug7(x, y, z, r, t, s, u) ;
+#define wait ;
+#define derr(x) ;
+#define derr2(x, y) ;
+#define derr3(x, y, z) ;
+#define derr4(x, y, z, q) ;
+#define derr5(x, y, z, r, t) ;
+#define derr6(x, y, z, r, t, s) ;
+#define derr7(x, y, z, r, t, f, u) ;
+#define checkpoint ;
+#endif
+
+#define print_matrix(a, n, m) \
+    REPE(0, n, 0, m) { cout << (a)[i][j] << ((j == m - 1) ? '\n' : ' '); }
+#define present(container, element) (container.find(element) != container.end())
+#define endl "\n"
+template <typename T>
+ostream &operator<<(ostream &o, vector<T> v)
+{
+    if (v.size() > 0)
+        o << v[0];
+    for (unsigned i = 1; i < v.size(); i++)
+        o << " " << v[i];
+    return o << " ";
+}
+template <typename U, typename V>
+ostream &operator<<(ostream &o, pair<U, V> p)
+{
+    return o << "(" << p.first << ", " << p.second << ") ";
+}
+template <typename T>
+istream &operator>>(istream &in, vector<T> &v)
+{
+    for (unsigned i = 0; i < v.size(); i++)
+        in >> v[i];
+    return in;
+}
+template <typename T>
+istream &operator>>(istream &in, pair<T, T> &p)
+{
+    in >> p.X;
+    in >> p.Y;
+    return in;
+}
+template <typename T>
+ostream &operator<<(ostream &o, set<T> v)
+{
+    TRV(v)
+    o << it << " ";
+    return o << endl;
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &o, map<T, U> v)
+{
+    TRV(v)
+    o << it << " ";
+    return o << endl;
+}
+template <typename T>
+T &&vmin(T &&val)
+{
+    return std::forward<T>(val);
+}
+
+template <typename T0, typename T1, typename... Ts>
+auto vmin(T0 &&val1, T1 &&val2, Ts &&... vs)
+{
+    return (val1 < val2) ? vmin(val1, std::forward<Ts>(vs)...) : vmin(val2, std::forward<Ts>(vs)...);
+}
+template <typename T>
+T &&vmax(T &&val)
+{
+    return std::forward<T>(val);
+}
+
+template <typename T0, typename T1, typename... Ts>
+auto vmax(T0 &&val1, T1 &&val2, Ts &&... vs)
+{
+    return (val1 > val2) ? vmax(val1, std::forward<Ts>(vs)...) : vmax(val2, std::forward<Ts>(vs)...);
+}
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+int main(int argc, char *argv[])
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cout.precision(20);
+
+    lo n;
+    cin >> n;
+    vl a(n);
+    cin >> a;
+    vl left(n);
+    vl right(n);
+    stack<lo> S;
+    S.push(0);
+    left[0] = a[0];
+    REP(1, n)
+    {
+        if (a[S.top()] > a[i])
+        {
+            while (a[S.top()] > a[i])
+            {
+                S.pop();
+                if (S.empty())
+                    break;
+            }
+            if (S.empty())
+                left[i] = a[i] * (i + 1);
+            else
+                left[i] = left[S.top()] + a[i] * (i - S.top());
+            S.push(i);
+        }
+        else
+        {
+            left[i] = left[i - 1] + a[i];
+            S.push(i);
+        }
+    }
+    reverse(all(a));
+    right[0] = a[0];
+    while (!S.empty())
+        S.pop();
+    S.push(0);
+    REP(1, n)
+    {
+        if (a[S.top()] > a[i])
+        {
+            while (a[S.top()] > a[i])
+            {
+                S.pop();
+                if (S.empty())
+                    break;
+            }
+            if (S.empty())
+                right[i] = a[i] * (i + 1);
+            else
+                right[i] = right[S.top()] + a[i] * (i - S.top());
+            S.push(i);
+        }
+        else
+        {
+            right[i] = right[i - 1] + a[i];
+            S.push(i);
+        }
+    }
+    reverse(all(right));
+    lo res = 0;
+    lo index = 0;
+    derr2(left, right);
+    REP(1, n)
+    {
+        if (res < left[i - 1] + right[i])
+        {
+            res = left[i - 1] + right[i];
+            index = i;
+        }
+    }
+    if (right[0] > res)
+        index = 0, res = right[0];
+    if (left.back() > res)
+        index = n;
+    checkpoint;
+    derr(index);
+    reverse(all(a));
+    while (!S.empty())
+        S.pop();
+    if (index > 0)
+    {
+        S.push(0);
+        REP(1, index)
+        {
+            if (a[S.top()] > a[i])
+            {
+                while (a[S.top()] > a[i])
+                {
+                    S.pop();
+                    if (S.empty())
+                        break;
+                }
+                S.push(i);
+            }
+            else
+            {
+                S.push(i);
+            }
+        }
+        vl ans;
+        while (!S.empty())
+        {
+            lo i = S.top();
+            S.pop();
+            lo j = 0;
+            if (!S.empty())
+                j = S.top() + 1;
+            REPV(k, j, i + 1)
+            ans.push_back(a[i]);
+        }
+        reverse(all(ans));
+        cout << ans;
+    }
+    if (index < n)
+    {
+        index = n - index;
+        reverse(all(a));
+        while (!S.empty())
+            S.pop();
+        S.push(0);
+        REP(1, index)
+        {
+            if (a[S.top()] > a[i])
+            {
+                while (a[S.top()] > a[i])
+                {
+                    S.pop();
+                    if (S.empty())
+                        break;
+                }
+                S.push(i);
+            }
+            else
+            {
+                S.push(i);
+            }
+        }
+        vl ans;
+        while (!S.empty())
+        {
+            lo i = S.top();
+            S.pop();
+            lo j = 0;
+            if (!S.empty())
+                j = S.top() + 1;
+            REPV(k, j, i + 1)
+             cout << a[i] << " ";
+        }
+        // reverse(all(ans));
+        // cout << ans;
+    }
+    return 0;
+}
